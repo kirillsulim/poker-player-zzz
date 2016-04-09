755bde717ea6a0a7538b1c8d0465f83f28cbd9ce
Kirill Sulim <kirillsulim@gmail.com>
2016-04-09 14:16:20 +0500

    a

--- player.py
+++ player.py
@@ -1,19 +1,35 @@
 import sys
 
 class Player:
-    VERSION = "zzz-print-9999-getp"
+    VERSION = "zzz-print-9999-getp-hand"
 
     def get_player(self, game_state, name):
         for player in game_state.players:
             if player.name == name:
                 return player
 
+    def get_hand(self, player):
+        return player.hole_cards
+
+    def is_ace(self, cards):
+        result = ""
+        for card in cards:
+            result += card.rank
+
+        if cards[0].suit == cards[1].suit:
+            result += 'S'
+        else:
+            result += 'O'
+
     def betRequest(self, game_state):
         try:
             print 'a'
             print game_state
             print 'me'
-            print get_player(self, game_state, self.VERSION)
+            player = self.get_player(game_state, self.VERSION)
+            print player
+            print 'hand'
+            print self.get_hand(player)
             sys.stdout.write(game_state)
             sys.stderr.write(game_state)
             return 9999
@@ -22,6 +38,8 @@ class Player:
 
     def showdown(self, game_state):
         pass
+
+
 
 
 
