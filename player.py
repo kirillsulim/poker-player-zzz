bc04e182a0b5586f5ce97eeac7b09861471719ed
Kirill Sulim <kirillsulim@gmail.com>
2016-04-09 14:08:41 +0500

    a

--- player.py
+++ player.py
@@ -1,12 +1,19 @@
 import sys
 
 class Player:
-    VERSION = "zzz-print-9999"
+    VERSION = "zzz-print-9999-getp"
+
+    def get_player(self, game_state, name):
+        for player in game_state.players:
+            if player.name == name:
+                return player
 
     def betRequest(self, game_state):
         try:
             print 'a'
             print game_state
+            print 'me'
+            print get_player(self, game_state, self.VERSION)
             sys.stdout.write(game_state)
             sys.stderr.write(game_state)
             return 9999
@@ -15,4 +22,6 @@ class Player:
 
     def showdown(self, game_state):
         pass
+
+
 
